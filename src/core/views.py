742cf19e9bf56d41f154fa346ed40d0a42e408cb
ton-s <sohyantonin@gmail.com>
2023-10-31 14:26:53 +0100

    test weather api

--- src/core/views.py
+++ src/core/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+import requests
 
 def index(request):
+
+    # key = "3448cb1dd62b9758038f490877c25fb6"
+    # url = f"http://api.weatherstack.com/current?access_key={key}&query=Gedinne"
+    #
+    #
+    # data = requests.get(url).json()
+    #
+    # print(data)
 
     return render(request, 'base.html')
