6226958ac764644cf0ad54ce47ef300956f55503
ton-s <sohyantonin@gmail.com>
2023-11-03 17:51:26 +0100

    add Room table

--- src/rooms/models.py
+++ src/rooms/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
-# Create your models here.
+from thermo.settings import AUTH_USER_MODEL
+
+
+class Room(models.Model):
+    name = models.CharField(max_length=128)
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='rooms')
