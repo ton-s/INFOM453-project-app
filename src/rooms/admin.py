c8db185360afc6354663939cc5b7dcb7f4dddfd3
ton-s <sohyantonin@gmail.com>
2023-11-04 17:37:38 +0100

    update BD

--- src/rooms/admin.py
+++ src/rooms/admin.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
 
-from rooms.models import Room
+from rooms.models import Room, Device, Lighting, Heating, HomeAppliance, Notification
 
 # Register the models
 class RoomAdmin(admin.ModelAdmin):
     list_display = ('name',)
 
 admin.site.register(Room, RoomAdmin)
+admin.site.register(Device)
+admin.site.register(Lighting)
+admin.site.register(Heating)
+admin.site.register(HomeAppliance)
+admin.site.register(Notification)
--- src/rooms/models.py
+++ src/rooms/models.py
@@ -16,3 +16,52 @@ class Room(models.Model):
         if not self.slug:
             self.slug = slugify(self.name)
         super(Room, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return f"{self.name}"
+
+class Device(models.Model):
+    type = models.CharField(max_length=128)
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='devices')
+
+    def __str__(self):
+        return f"{self.type}"
+
+class Lighting(models.Model):
+    brightness_outside = models.FloatField()
+    brightness_inside = models.FloatField()
+    brightness_desired = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    devices = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='lightings')
+
+    def __str__(self):
+        return f"{self.brightness_inside} ({self.timestamp})"
+class Heating(models.Model):
+    temperature_outside = models.FloatField()
+    temperature_inside = models.FloatField()
+    temperature_desired = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    devices = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='heating')
+
+
+    def __str__(self):
+        return f"{self.temperature_inside} ({self.timestamp})"
+
+
+class HomeAppliance(models.Model):
+    mode = models.CharField(max_length=128)
+    power = models.FloatField()
+    timestamp = models.DateTimeField(auto_now_add=True)
+    devices = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='HomeAppliances')
+
+    def __str__(self):
+        return f"{self.mode}-{self.power} ({self.timestamp})"
+
+
+class Notification(models.Model):
+    content = models.CharField(max_length=512)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    devices = models.ForeignKey(Device, on_delete=models.CASCADE, related_name='notifications')
+
+    def __str__(self):
+        return f"{self.id}: {self.content} ({self.timestamp})"
