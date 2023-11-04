8e0ade60c3402d09e8e1cf4636adccf657e45c4d
ton-s <sohyantonin@gmail.com>
2023-11-04 12:55:44 +0100

    update BD

--- src/rooms/admin.py
+++ src/rooms/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
+from rooms.models import Room
+
+# Register the models
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ('name',)
+
+admin.site.register(Room, RoomAdmin)
--- src/rooms/models.py
+++ src/rooms/models.py
@@ -1,8 +1,18 @@
 from django.db import models
+from django.utils.text import slugify
 
 from thermo.settings import AUTH_USER_MODEL
 
 
 class Room(models.Model):
     name = models.CharField(max_length=128)
+    slug = models.SlugField(max_length=128, unique=True)
     user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='rooms')
+
+    def save(self, *args, **kwargs):
+        """
+        Save the new room with a unique slug
+        """
+        if not self.slug:
+            self.slug = slugify(self.name)
+        super(Room, self).save(*args, **kwargs)
