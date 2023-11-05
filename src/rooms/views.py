84fbe7d9a023d511774d3bb7a4977e99677ecfd7
ton-s <sohyantonin@gmail.com>
2023-11-05 22:03:29 +0100

    update interface + test chart

--- src/rooms/views.py
+++ src/rooms/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+import random
+
 
 def room(request, slug):
+    datapoints = []
+    count, y = 24, 5
 
+    for i in range(count):
+        y += round(5 + random.uniform(-5, 5))
+        datapoints.append({"x": i, "y": y})
 
-    return render(request, 'rooms/room.html', context={})
+    print(datapoints)
 
+    return render(request, 'rooms/room.html', context={"datapoints": datapoints})
