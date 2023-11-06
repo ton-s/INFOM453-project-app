ed6699df4e38d3ddf4f6161653b4be034b22490e
ton-s <sohyantonin@gmail.com>
2023-11-06 20:44:02 +0100

    correction

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -1,34 +1,37 @@
-// Burger menu
+window.onload = function () {
+    // Burger menu
 
-const menu = document.getElementById("menu");
-const openBtn = document.getElementById("burger-menu-open");
-const closeBtn = document.getElementById("burger-menu-close");
+    const menu = document.getElementById("menu");
+    const openBtn = document.getElementById("burger-menu-open");
+    const closeBtn = document.getElementById("burger-menu-close");
 
-openBtn.onclick = function () {
-    menu.classList.add("active");
-}
+    openBtn.onclick = function () {
+        menu.classList.add("active");
+    }
 
-closeBtn.onclick = function () {
-    menu.classList.remove("active");
-}
+    closeBtn.onclick = function () {
+        menu.classList.remove("active");
+    }
 
 // Btn day/night
 
-const dayNightBtn = document.getElementById("day-night");
+    const dayNightBtn = document.getElementById("day-night");
 
-if (dayNightBtn) {
-    dayNightBtn.onclick = function () {
+    if (dayNightBtn) {
+        dayNightBtn.onclick = function () {
 
-        const content = dayNightBtn.querySelector("i");
+            const content = dayNightBtn.querySelector("i");
 
-        if (!dayNightBtn.classList.toggle("day-night-active")) {
-            content.outerHTML = "<i class=\"fa-solid fa-sun fa-xl\"></i>";
-        } else {
-            content.outerHTML = "<i class=\"fa-regular fa-moon fa-xl\"></i>";
+            if (!dayNightBtn.classList.toggle("day-night-active")) {
+                content.outerHTML = "<i class=\"fa-solid fa-sun fa-xl\"></i>";
+            } else {
+                content.outerHTML = "<i class=\"fa-regular fa-moon fa-xl\"></i>";
+            }
+
         }
-
     }
 }
+
 
 
 
