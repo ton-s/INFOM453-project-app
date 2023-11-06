06e728322e398a946ccd396706327d7d7eb905b3
ton-s <sohyantonin@gmail.com>
2023-11-06 11:09:42 +0100

    weather api

---
+++ src/core/utils.py
@@ -0,0 +1,21 @@
+import requests
+
+
+def get_weather_data(city):
+    """Retrieves current weather data for a given city
+
+    Parameters
+    ----------
+    city (str): Name of the city for which you wish to obtain weather data.
+
+    Returns:
+        weather_data (dict): Current weather data in JSON format.
+    """
+    key = "3448cb1dd62b9758038f490877c25fb6"  # Pas bonne pratique
+
+    url = f"http://api.weatherstack.com/current?access_key={key}&query={city}"
+
+    # Send HTTP GET request to URL and convert JSON response into a dictionary
+    weather_data = requests.get(url).json()
+
+    return weather_data
--- src/core/views.py
+++ src/core/views.py
@@ -1,19 +1,13 @@
 from django.shortcuts import render
-import requests
 
+from core.utils import get_weather_data
 from rooms.models import Room
 
 
 def index(request):
 
-    # key = "3448cb1dd62b9758038f490877c25fb6"
-    # url = f"http://api.weatherstack.com/current?access_key={key}&query=Gedinne"
-    #
-    #
-    # data = requests.get(url).json()
-    #
-    # print(data)
+    weather = get_weather_data("Namur")
 
     rooms = Room.objects.all()
 
-    return render(request, 'core/home.html', context={'rooms': rooms})
+    return render(request, 'core/home.html', context={'rooms': rooms, 'weather': weather})
