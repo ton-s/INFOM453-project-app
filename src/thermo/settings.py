09787bab8f0eff20a2514b77721d1ca8892aac6c
ton-s <sohyantonin@gmail.com>
2023-11-06 20:44:20 +0100

    reset hosts

--- src/thermo/settings.py
+++ src/thermo/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-36ljbgq-=zo8tx%7_(+k5q(hgxm_r1hsvxk5we8h73@c&pjaob
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = []
 
 
 # Application definition
