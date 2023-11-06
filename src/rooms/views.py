4dc848b00010e59791ef8d5e1f0297ccd31e8f7e
ton-s <sohyantonin@gmail.com>
2023-11-06 11:09:56 +0100

    update chart (test)

--- src/rooms/views.py
+++ src/rooms/views.py
@@ -4,12 +4,13 @@ import random
 
 def room(request, slug):
     datapoints = []
+    datapoints2 = []
     count, y = 24, 5
 
     for i in range(count):
         y += round(5 + random.uniform(-5, 5))
         datapoints.append({"x": i, "y": y})
+        datapoints2.append({"x": i+2, "y": y-3})
 
-    print(datapoints)
 
-    return render(request, 'rooms/room.html', context={"datapoints": datapoints})
+    return render(request, 'rooms/room.html', context={"datapoints": datapoints, "datapoints2": datapoints2})
