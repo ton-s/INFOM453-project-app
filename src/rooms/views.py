12ab4390cb210d0d457addd3f8d12ba78d1dd52c
ton-s <sohyantonin@gmail.com>
2023-11-07 15:41:50 +0100

    correction chart

--- src/rooms/views.py
+++ src/rooms/views.py
@@ -12,23 +12,15 @@ def room(request, slug):
     lighting_data = room.d_lighting.lighting_data
     today = timezone.now().date()
 
-    # chart_data_1 = []
-    # chart_data_1_threshold = []
-    # count, y = 24, 5
-    #
-    # for i in range(count):
-    #     y += round(5 + random.uniform(-5, 5))
-    #     chart_data_1.append({"x": i, "y": y})
-    #     chart_data_1_threshold.append({"x": i + 2, "y": y - 3})
-
     chart_data_1 = []
     chart_data_1_threshold = []
     # chart_data_2 = []
 
     for data in heating_data.filter(timestamp__date=today):
-        chart_data_1.append({"x": int(data.timestamp.timestamp()), "y": data.temperature_inside})
-        chart_data_1_threshold.append({"x": int(data.timestamp.timestamp()), "y": data.temperature_outside})
-    print(chart_data_1)
+        timestamp_ms = int(data.timestamp.timestamp()) * 1000  # Format Unix
+        chart_data_1.append({"x": timestamp_ms, "y": data.temperature_inside})
+        chart_data_1_threshold.append({"x": timestamp_ms, "y": data.temperature_outside})
+
     context = {
         "room": room,
         "temperature_outside": int(heating_data.last().temperature_outside),
