98ce7f339c2abd05fa9f8d8a3294e5c529d5b9ab
ton-s <sohyantonin@gmail.com>
2023-11-13 13:36:09 +0100

    config consumption

--- src/thermo/urls.py
+++ src/thermo/urls.py
@@ -20,5 +20,6 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('core.urls')),
     path('', include('rooms.urls')),
+    path('', include('consumption.urls')),
     path('admin/', admin.site.urls),
 ]
