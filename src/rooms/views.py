b8b224fee13ebb295f793e666a9db237cabbcc50
ton-s <sohyantonin@gmail.com>
2023-11-14 14:17:57 +0100

    correction error

--- src/rooms/views.py
+++ src/rooms/views.py
@@ -11,30 +11,36 @@ def room(request, slug):
     room = get_object_or_404(Room, slug=slug)
     heating_data = room.d_heating.heating_data
     lighting_data = room.d_lighting.lighting_data
+    if heating_data.exists() and lighting_data.exists():
 
-    # Prepare data (today) for chart
-    chart_data_1, chart_data_1_threshold = prepare_data(heating_data,
-                                                        "temperature_inside",
-                                                        "temperature_outside")
-    chart_data_2, chart_data_2_threshold = prepare_data(lighting_data,
-                                                        "brightness_inside",
-                                                        "brightness_outside")
+        # Prepare data (today) for chart
+        chart_data_1, chart_data_1_threshold = prepare_data(heating_data,
+                                                            "temperature_inside",
+                                                            "temperature_outside")
+        chart_data_2, chart_data_2_threshold = prepare_data(lighting_data,
+                                                            "brightness_inside",
+                                                            "brightness_outside")
 
-    # messages.info(request, "Votre chauffage veut augmenter la température de 5°C", extra_tags="Chauffage")
+        # messages.info(request, "Votre chauffage veut augmenter la température de 5°C", extra_tags="Chauffage")
 
-    context = {
-        "room": room,
-        "temperature_outside": heating_data.last().temperature_outside,
-        "temperature_inside": heating_data.last().temperature_inside,
-        "brightness_outside": lighting_data.last().brightness_outside,
-        "brightness_inside": LightingData.convert_lumen_to_percent(lighting_data.last().brightness_inside),
-        "chart_data_1": chart_data_1,
-        "chart_data_1_threshold": chart_data_1_threshold,
-        "chart_data_2": chart_data_2,
-        "chart_data_2_threshold": chart_data_2_threshold,
-    }
+        context = {
+            "room": room,
+            "temperature_outside": heating_data.last().temperature_outside,
+            "temperature_inside": heating_data.last().temperature_inside,
+            "brightness_outside": lighting_data.last().brightness_outside,
+            "brightness_inside": LightingData.convert_lumen_to_percent(lighting_data.last().brightness_inside),
+            "chart_data_1": chart_data_1,
+            "chart_data_1_threshold": chart_data_1_threshold,
+            "chart_data_2": chart_data_2,
+            "chart_data_2_threshold": chart_data_2_threshold,
+        }
 
-    return render(request, 'rooms/room.html', context=context)
+        return render(request, 'rooms/room.html', context=context)
+
+    else:
+        # Indicates that the request is not allowed
+        return HttpResponseBadRequest("Pas de données disponibles")
+
 
 
 def increase_temperature(request, slug):
