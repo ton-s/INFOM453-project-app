ec5dd81afab4a673e7562c1a99185b01f7ff7f73
ton-s <sohyantonin@gmail.com>
2023-11-14 02:26:53 +0100

    change key (weather API)

--- src/core/utils.py
+++ src/core/utils.py
@@ -11,11 +11,14 @@ def get_weather_data(city: str = "Namur"):
     Returns:
         weather_data (dict): Current weather data in JSON format.
     """
-    key = "3448cb1dd62b9758038f490877c25fb6"  # Pas bonne pratique
+    key = "25cd8ec6f4c2c912b8b5bec00cc2795c"  # Pas bonne pratique
 
     url = f"http://api.weatherstack.com/current?access_key={key}&query={city}"
 
     # Send HTTP GET request to URL and convert JSON response into a dictionary
     weather_data = requests.get(url).json()
+
+    if "error" in weather_data:
+      print("Expired key !!!!")
 
     return weather_data
