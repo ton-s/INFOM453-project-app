ed213bbc766eb0d5129e67c0dfbacab2fff2013a
ton-s <sohyantonin@gmail.com>
2023-11-14 01:33:10 +0100

    correction migrate

--- src/rooms/migrations/0002_homeappliancedata_time_work.py
+++ src/rooms/migrations/0002_homeappliancedata_time_work.py
@@ -13,7 +13,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='homeappliancedata',
             name='time_work',
-            field=models.FloatField(default=''),
+            field=models.FloatField(default=0),
             preserve_default=False,
         ),
     ]
