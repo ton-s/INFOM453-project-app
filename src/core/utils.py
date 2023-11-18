e4cd8a0362d449934bffc2fda203981e08c431cd
ton-s <sohyantonin@gmail.com>
2023-11-18 15:01:25 +0100

    add calculating seasons

--- src/core/utils.py
+++ src/core/utils.py
@@ -1,7 +1,8 @@
 import requests
+from datetime import date
 
 
-def get_weather_data(city: str = "Namur"):
+def get_weather_data(city="Namur"):
     """Retrieves current weather data for a given city
 
     Parameters
@@ -19,6 +20,36 @@ def get_weather_data(city: str = "Namur"):
     weather_data = requests.get(url).json()
 
     if "error" in weather_data:
-      print("Expired key !!!!")
+        print("Expired key !!!!")
 
     return weather_data
+
+
+def get_season_now():
+    """Get the current season based on the current date.
+
+    Returns
+    -------
+    season (str): A string representing the current season ("spring", "summer", "autumn", or "winter")
+
+    Raises
+    ------
+    IndexError: If the calculated value doesn't match any known season.
+    """
+    now = date.today()
+    month = now.month * 100
+    day = now.day
+    month_day = month + day  # combining month and day
+
+    if (month_day >= 301) and (month_day <= 531):
+        season = "printemps"
+    elif (month_day > 531) and (month_day < 901):
+        season = "été"
+    elif (month_day >= 901) and (month_day <= 1130):
+        season = "automne"
+    elif (month_day > 1130) and (month_day <= 229):
+        season = "hiver"
+    else:
+        raise IndexError("Invalid")
+
+    return season
--- src/core/views.py
+++ src/core/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 
-from core.utils import get_weather_data
+from core.utils import get_weather_data, get_season_now
 from rooms.models import Room
 
 
 def index(request):
     weather = get_weather_data()
-
+    season = get_season_now()
     rooms = Room.objects.all()
 
-    return render(request, 'core/home.html', context={'rooms': rooms, 'weather': weather})
+    return render(request, 'core/home.html', context={'rooms': rooms, 'weather': weather, 'season': season})
