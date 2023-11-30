c305765ba2b5230aa5f408ba5df2cdfe9bfab69f
ton-s <sohyantonin@gmail.com>
2023-11-30 21:35:25 +0100

    change model

--- machine-learning/heating/poly-regression-learning.py
+++ machine-learning/heating/poly-regression-learning.py
@@ -2,6 +2,7 @@ from sklearn.model_selection import train_test_split
 from sklearn.metrics import mean_squared_error, r2_score
 import pandas as pd
 from sklearn.linear_model import LinearRegression
+from sklearn.preprocessing import PolynomialFeatures
 import pickle
 import os
 import matplotlib.pyplot as plt
@@ -19,16 +20,21 @@ target = data['temperature']
 # Divised the dataset into training and test sets
 X_train, X_test, y_train, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
 
+# Create polynomial features
+poly = PolynomialFeatures(degree=2)
+X_train_poly = poly.fit_transform(X_train.values)
+X_test_poly = poly.transform(X_test.values)
+
 # Create the model
 model = LinearRegression()
 
 # Train the model
-model.fit(X_train.values, y_train.values)
+model.fit(X_train_poly, y_train)
 
-print(model.score(X_train, y_train))
+print(model.score(X_train_poly, y_train))
 
 # Make predictions
-predictions = model.predict(X_test)
+predictions = model.predict(X_test_poly)
 
 # Evaluate the model
 mse = mean_squared_error(y_test, predictions)
@@ -44,7 +50,8 @@ plt.ylabel("Prédictions")
 plt.show()
 
 # Make a prediction
-result = round(model.predict([[0, 0, 0, 2, 4]])[0])
+sample_data_poly = poly.transform([[0, 0, 0, 2, 4]]) 
+result = round(model.predict(sample_data_poly)[0])
 
 print(f"Prédiction : {result}")
 
