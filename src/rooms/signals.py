08d4d01d62aef2b218e875c9452a71f76445727e
ton-s <sohyantonin@gmail.com>
2023-12-04 22:14:52 +0100

    update algo

--- src/rooms/signals.py
+++ src/rooms/signals.py
@@ -78,33 +78,57 @@ def heating_detection_algorithm(sender, instance, created, **kwargs):
 
     if not created:
         last_heating = sender.objects.filter(heating_id=instance.heating_id).exclude(id=instance.id).last()
-        print(last_heating)
+
         if last_heating:
+            # add a bias to the algorithm trigger
+            temp_change_outside = abs(float(instance.temperature_outside) - last_heating.temperature_outside)
+            temp_change_inside = abs(float(instance.temperature_inside) - last_heating.temperature_inside)
+            temp_change_desired = abs(float(instance.temperature_desired) - last_heating.temperature_desired)
 
-            prediction = run_model_heating(instance)
-            print(prediction)
+            if temp_change_outside > 2 or temp_change_inside > 2 or temp_change_desired > 2:
 
-            # create a heating notification
-            content = f"Salut, c'est moi !\nJe souhaite changer la température de ton chauffage à {prediction}°C"
-            action = (prediction - float(instance.temperature_desired))
-            context = {"content": content, "action": action, "heating": instance.heating}
+                prediction = run_model_heating(instance)
+                # print(last_heating)
+                # print(prediction)
 
-            last_instance, create = Notification.objects.get_or_create(heating=instance.heating, defaults=context)
+                # create a heating notification
+                content = f"Salut, c'est moi !\nJe souhaite changer la température de ton chauffage à {prediction}°C"
+                action = (prediction - float(instance.temperature_desired))
+                context = {"content": content, "action": action, "heating": instance.heating}
 
-            if not create:
-                last_instance.__dict__.update(**context)
-                last_instance.save()
+                last_instance, create = Notification.objects.get_or_create(heating=instance.heating, defaults=context)
 
-            #TODO - mettre une régle pour limiter l'envoie de notif
+                if not create:
+                    last_instance.__dict__.update(**context)
+                    last_instance.save()
+
+
 @receiver(post_save, sender=LightingData)
 def lighting_detection_algorithm(sender, instance, created, **kwargs):
     """Algorithm for detecting the ideal brightness in a room"""
 
     if not created:
         last_lighting = sender.objects.filter(lighting_id=instance.lighting_id).exclude(id=instance.id).last()
-        print(last_lighting)
-        if last_lighting:
-            prediction = run_model_lighting(instance)
-            print(f"Lighting: {prediction}")
 
-            # create a lighting notification
+        if last_lighting:
+            # add a bias to the algorithm trigger
+            brightness_change_outside = abs(float(instance.brightness_outside) - last_lighting.brightness_outside)
+            brightness_change_inside = abs(float(instance.brightness_inside) - last_lighting.brightness_inside)
+
+            if brightness_change_outside >= (LightingData.MAX_BRIGHTNESS_OUTSIDE * 0.2) \
+                    or brightness_change_inside >= (LightingData.MAX_BRIGHTNESS_INSIDE * 0.1):
+
+                prediction = run_model_lighting(instance)
+                print(last_lighting)
+                print(f"Lighting: {prediction}")
+
+                # create a lighting notification
+                content = f"Salut, c'est moi !\nJe souhaite changer la luminosité de la pièce à {prediction}"
+                action = prediction
+                context = {"content": content, "action": action, "lighting": instance.lighting}
+
+                last_instance, create = Notification.objects.get_or_create(lighting=instance.lighting, defaults=context)
+
+                if not create:
+                    last_instance.__dict__.update(**context)
+                    last_instance.save()
