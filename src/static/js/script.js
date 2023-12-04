5ec540aa6cc3902cfa738976197d3ecb80e7f110
ton-s <sohyantonin@gmail.com>
2023-12-04 18:05:47 +0100

    management of seasons display

--- src/static/js/script.js
+++ src/static/js/script.js
@@ -12,7 +12,7 @@ closeBtn.onclick = function () {
     menu.classList.remove("active");
 }
 
-// Btn day/night
+// Btn day/night mode
 
 const dayNightBtn = document.getElementById("day-night");
 
@@ -29,6 +29,41 @@ if (dayNightBtn) {
 
     }
 }
+
+// Widget Season
+
+const season = document.getElementById("data_season");
+const seasonWidget = document.getElementById("season");
+const seasonIcon = seasonWidget.querySelector(".type");
+
+const seasonText = season.textContent.replace(/"/g, "");
+
+const seasonData = {
+    printemps: {
+        color: "#2ecc71",
+        icon: '<i class="fa-solid fa-seedling fa-sm"></i>'
+    },
+    été: {
+        color: "#1abc9c",
+        icon: '<i class="fa-solid fa-umbrella-beach fa-sm"></i>'
+    },
+    automne: {
+        color: "brown",
+        icon: '<i class="fa-solid fa-leaf fa-sm"></i>'
+    },
+    default: {
+        color: "#2c3e50",
+        icon: '<i class="fa-solid fa-snowflake fa-sm"></i>'
+    }
+}
+
+const chosenSeason = seasonData[seasonText] || seasonData.default
+seasonWidget.style.backgroundColor = chosenSeason.color
+seasonIcon.innerHTML = chosenSeason.icon
+
+
+
+
 
 
 
