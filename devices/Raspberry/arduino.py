ce198030e9bd87f223c4479f449218210df44168
sdourte <57835302+sdourte@users.noreply.github.com>
2023-12-17 18:23:54 +0100

    Update arduino.py

--- devices/Raspberry/arduino.py
+++ devices/Raspberry/arduino.py
@@ -1,17 +1,23 @@
 import serial
 import time
 
-def send_intensity(intensity_led1, intensity_led2, intensity_led3):
+def send_intensity(room, intensity_led, action_motor):
     try:
-        ser = serial.Serial('/dev/ttyACM0', 9600)  # Assurez-vous de remplacer '/dev/ttyUSB0' par le bon port série
-        time.sleep(2)  # Attendez que la connexion soit établie
-        intensities = [intensity_led1, intensity_led2, intensity_led3]
-        ser.write(bytes(intensities))  # Envoyer les intensités à l'Arduino
+        ser = serial.Serial('/dev/ttyACM0', 9600)
+        time.sleep(2)
+
+        # Création d'une liste avec les données
+        data = [room, intensity_led, action_motor]
+
+        # Envoyer les données à l'Arduino
+        ser.write(bytes(str(data)))
+
     except serial.SerialException as e:
         print(f"Une erreur s'est produite : {e}")
-    # Pour être sûr que la connexion se ferme bien si une exception arrive ou pas
+
     finally:
+        # Fermer la connexion série
         ser.close()
 
-# Exemple d'utilisation avec des valeurs d'intensité arbitraires
-send_intensity(50, 75, 100)
+# Exemple d'utilisation avec des valeurs arbitraires
+send_intensity("chambre", 75, "false")  # Chambre 1, luminosité 75, rideau fermé
