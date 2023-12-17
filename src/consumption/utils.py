e8b5d18f687ff4f2f4d2b3a1af9896e4b7a7fa0b
ton-s <sohyantonin@gmail.com>
2023-12-17 18:38:36 +0100

    partie Piras

--- src/consumption/utils.py
+++ src/consumption/utils.py
@@ -56,3 +56,24 @@ def prepare_data_consumption_home_appliance():
             })
 
     return data
+
+
+def heating_consumption_calculator(heating_type, current_temp, target_temp, room_volume, starting_time):
+    # Does not take into account the insulation of the room and the outside temperature
+    now = datetime.datetime.now()
+    heating_time = now - starting_time
+    heating_time = heating_time.total_seconds() / 3600  # in hours
+
+    if heating_type == 'oil':
+        mean_lower_heating_volume = 9.96  # in kWh for 1L of oil
+
+    # calculate the power needed to heat the room
+    air_volumetric_eat_capacity = 1256  # in J/(m3⋅°C)
+    heating_power_needed = (target_temp - current_temp) * air_volumetric_eat_capacity * room_volume  # in J
+    heating_power_needed = heating_power_needed / 3600000  # in kilowatts
+
+    # calculate the consumption
+    print(heating_time, heating_power_needed)
+    consumption = (heating_power_needed * heating_time) / mean_lower_heating_volume  # in L
+
+    return consumption
--- src/consumption/views.py
+++ src/consumption/views.py
@@ -1,13 +1,14 @@
 from django.shortcuts import render
 
-from consumption.utils import prepare_data_consumption_lighting, prepare_data_consumption_home_appliance
+from consumption.utils import prepare_data_consumption_lighting, prepare_data_consumption_home_appliance, \
+    heating_consumption_calculator
+from rooms.models import Room
 
 
 def electric_consumption(request):
     # electric global
     global_lighting_chart_data = prepare_data_consumption_lighting(0, 9)
     global_lighting_chart_data_threshold = prepare_data_consumption_lighting(8, 10)
-
 
     context = {
         'global_lighting_chart_data': global_lighting_chart_data,
@@ -19,4 +20,35 @@ def electric_consumption(request):
 
 
 def heating_consumption(request):
-    return render(request, 'consumption/heating.html')
+    heating_consumption = 0
+    rooms_heating_datas = {}
+    all_rooms = Room.objects.all()
+    if all_rooms.exists():
+        for room in all_rooms:
+            heating_datas = room.d_heating.heating_data.all()
+            if heating_datas.exists():
+                rooms_heating_datas[room.name] = []
+                for heating_data in heating_datas:
+                    temperature_inside = heating_data.temperature_inside
+                    temperature_targeted = heating_data.temperature_desired
+                    starting_time = heating_data.timestamp
+                    room_volume = 30 * 2.5  # room surface * room height
+                    heating_type = 'oil'
+                    heating_consumption = heating_consumption_calculator(heating_type, temperature_inside,
+                                                                         temperature_targeted, room_volume,
+                                                                         starting_time)
+
+                    rooms_heating_datas[room.name].append(
+                        {'x': int(starting_time.timestamp()) * 1000, 'y': heating_consumption})
+
+    colors = [
+        "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
+        "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
+        "#aec7e8", "#ffbb78", "#98df8a", "#ff9896", "#c5b0d5",
+        "#c49c94", "#f7b6d2", "#c7c7c7", "#dbdb8d", "#9edae5",
+        "#393b79", "#637939", "#8c6d31", "#843c39", "#7b4173",
+        "#5254a3", "#637939", "#8c6d31", "#843c39", "#7b4173",
+    ]
+    context = {"heating_data": rooms_heating_datas, "colors": colors}
+
+    return render(request, 'consumption/heating.html', context=context)
