5b47db7f104bcdd47b19d73ae87e4504076d123c
ton-s <sohyantonin@gmail.com>
2023-12-20 11:51:07 +0100

    refactoring

--- src/consumption/views.py
+++ src/consumption/views.py
@@ -23,16 +23,7 @@ def heating_consumption(request):
    
     all_rooms = Room.objects.all()
     heating_datas = prepare_data_consumption_heating(all_rooms)
-   
-    print(heating_datas)
-    colors = [
-        "#1f77b4", "#ff7f0e", "#2ca02c", "#d62728", "#9467bd",
-        "#8c564b", "#e377c2", "#7f7f7f", "#bcbd22", "#17becf",
-        "#aec7e8", "#ffbb78", "#98df8a", "#ff9896", "#c5b0d5",
-        "#c49c94", "#f7b6d2", "#c7c7c7", "#dbdb8d", "#9edae5",
-        "#393b79", "#637939", "#8c6d31", "#843c39", "#7b4173",
-        "#5254a3", "#637939", "#8c6d31", "#843c39", "#7b4173",
-    ]
-    context = {"heating_data": heating_datas, "colors": colors}
+
+    context = {"heating_data": heating_datas}
 
     return render(request, 'consumption/heating.html', context=context)
